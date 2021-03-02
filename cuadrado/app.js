9ba5de6c1fc3d82d11657644ba02f94fd0fecdfb
KeilinPB <kmariapb@gmail.com>
2021-03-01 19:40:19 -0600

    Agregando ejercicio

---
+++ cuadrado/app.js
@@ -0,0 +1,9 @@
+let base = prompt('Hola ingresa la base del cuadrado');
+let altura = prompt('Hola ingresa la altura del cuadrado');
+
+const area = (base, altura) => {
+    let resul = base * altura;
+    alert(resul);
+}
+
+area (base, altura);
